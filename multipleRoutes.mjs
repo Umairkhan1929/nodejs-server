a53e74ae23f0374a6ec1566c655a3cf171baec4f
Umairkhan1929 <umairkhanit1@gmial.com>
2024-10-03 22:03:53 +0500

    added / route means home page

--- multipleRoutes.mjs
+++ multipleRoutes.mjs
@@ -1,1 +1,19 @@
 import { createServer } from 'http'
+
+const port = 3000;
+
+const myServer = createServer((req, res)=>{
+    const url = req.url
+    const method = res.method
+
+    if(url === '/'){
+        res.writeHead(200, {'Content-Type': 'text/plain'})
+        res.end('This is the Home Page')
+    }
+
+
+})
+
+myServer.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`)
+})
